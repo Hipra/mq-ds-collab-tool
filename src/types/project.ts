69151290f46c8d1783d3805ea415f198f33ab8de
Gyorgy Bokros <gyorgy.bokros@gmail.com>
2026-03-13 13:37:22 +0100

    feat: add Project type definitions

---
+++ src/types/project.ts
@@ -0,0 +1,31 @@
+export type DesignStatus = 'concept' | 'in_progress' | 'review' | 'done';
+export type DevStatus = 'not_started' | 'in_progress' | 'qa' | 'deployed';
+export type UxWriterStatus = 'not_started' | 'in_progress' | 'review' | 'done';
+
+export interface Project {
+  id: string;
+  name: string;
+  description: string;
+  assignee: string;
+  designStatus: DesignStatus;
+  devStatus: DevStatus;
+  uxWriterStatus: UxWriterStatus;
+  prototypeIds: string[];
+  updatedAt: string;
+}
+
+export interface ScreenInfo {
+  id: string;
+  name: string;
+  hasThumbnail: boolean;
+}
+
+export interface PrototypeInfo {
+  id: string;
+  name: string;
+  screens: ScreenInfo[];
+}
+
+export interface ProjectWithPrototypes extends Project {
+  prototypes: PrototypeInfo[];
+}
